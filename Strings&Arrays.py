2eb9a2d6a95c14882a179fb38eadae8dfd009a00
kachup1 <emailkarlahere@gmail.com>
2025-02-22 22:37:26 -0800

    Added problem 4

--- Strings&Arrays.py
+++ Strings&Arrays.py
@@ -55,9 +55,27 @@ character = "Piglet"
 print_catchphrase(character)
 
 '''
+Problem 4: Return Item
 
+Implement a function get_item() that accepts a 0-indexed list items and a non-negative integer x and returns the element at index x in items. If x is not a valid index of items, return None.
 '''
+def get_item(items, x):
+    length = len(items)
 
+    if x >= length:
+        return print(None)
+    else:
+        return print(items[x])
+
+
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 2
+get_item(items, x)
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 5
+get_item(items, x)
 '''
 Problem 5: Total Honey
 
