47ae06bb88b9094663072c126fd087a60ccb999c
kachup1 <emailkarlahere@gmail.com>
2025-02-23 09:30:32 -0800

    section 2 problem 2

--- Strings&Arrays.py
+++ Strings&Arrays.py
@@ -125,3 +125,31 @@ reverse_sentence(sentence)
 
 sentence = "Pooh"
 reverse_sentence(sentence)
+
+'''
+Problem 2: Goldilocks Number
+In the extended universe of fictional bears, Goldilocks finds an enticing list of numbers in the Three Bears' house. She doesn't want to take a number that's too high or too low - she wants a number that's juuust right. Write a function goldilocks_approved() that takes in the list of distinct positive integers nums and returns any number from the list that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
+'''
+def goldilocks_approved(nums):
+    
+    length = len(nums)
+    just_right = 0
+
+    if length < 3:
+        return print(-1)
+    elif length % 2 == 0:
+        just_right = length//2
+        return print(just_right)
+    else:
+        just_right = (length // 2) + 1
+        return print(just_right)
+
+
+nums = [3, 2, 1, 4]
+goldilocks_approved(nums)
+
+nums = [1, 2]
+goldilocks_approved(nums)
+
+nums = [2, 1, 3]
+goldilocks_approved(nums)
